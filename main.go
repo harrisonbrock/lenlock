3f0e563f4d1bb4c957d333ae6801dceacef85ded
Harrison Brock <harrison@harrisonbrock.com>
2018-11-12 22:18:56 -0500

    Setup basic route

--- main.go
+++ main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
+func demoRoute(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Welcome to my site - change</h1>")
+}
+
 func main() {
-	fmt.Println("Hello!!!")
+	http.HandleFunc("/", demoRoute)
+	http.ListenAndServe(":3000", nil)
 }
