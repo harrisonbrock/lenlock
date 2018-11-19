15d77fa776ded4b204d375f1328fba72248ad5e2
Harrison Brock <harrison@harrisonbrock.com>
2018-11-18 21:59:59 -0500

    removed 404 route

--- main.go
+++ main.go
@@ -6,22 +6,6 @@ import (
 	"net/http"
 )
 
-//func handlerFunc(w http.ResponseWriter, r *http.Request) {
-//	w.Header().Set("Content-Type", "text/html")
-//	//fmt.Fprint(w,r.URL.Path)
-//	if r.URL.Path == "/" {
-//
-//		fmt.Fprint(w, "<h1>Welcome to my site - change</h1>")
-//	} else if r.URL.Path == "/contact" {
-//
-//		fmt.Fprint(w, "To get in touch, please send a email to <a href=\"mailto:support@gmail.com\">support@gmail.com</a>.")
-//	} else {
-//		w.WriteHeader(http.StatusNotFound)
-//		fmt.Fprint(w, "<h1>Page Not Found </h1>")
-//	}
-//
-//}
-
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>Welcome to my site - change</h1>")
@@ -31,12 +15,6 @@ func home(w http.ResponseWriter, r *http.Request) {
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "To get in touch, please send a email to <a href=\"mailto:support@gmail.com\">support@gmail.com</a>.")
-}
-
-func errorPage(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	w.WriteHeader(http.StatusNotFound)
-	fmt.Fprint(w, "<h1>Page Not Found </h1>")
 }
 
 func main() {
