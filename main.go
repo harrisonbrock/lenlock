005f813298fa6d4921d2170acbc842760ff5e29c
Harrison Brock <harrison@harrisonbrock.com>
2018-11-18 22:37:09 -0500

    added custom 404 for test

--- main.go
+++ main.go
@@ -21,8 +21,15 @@ func faq(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<p>Question 1</p> <p>Answer 1</p><p>Question 2</p> <p>Answer 2</p>")
 }
+
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1>Could find the page</h1>")
+}
 func main() {
 	router := mux.NewRouter()
+	router.NotFoundHandler = http.HandlerFunc(notFound)
 	router.HandleFunc("/", home)
 	router.HandleFunc("/contact", contact)
 	router.HandleFunc("/faq", faq)
