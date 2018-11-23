01a1eba67d63126d83f2ba613c6e93091dfe1ed5
harrisonbrock <harrison@harrisonbrock.com>
2018-11-23 12:04:15 -0500

    changed views to use the bootstrap layout

--- views/view.go
+++ views/view.go
@@ -2,9 +2,11 @@ package views
 
 import "html/template"
 
-func NewView(files ...string) *View {
+func NewView(layout string, files ...string) *View {
 
-	files = append(files, "views/layouts/footer.gohtml")
+	files = append(files,
+		"views/layouts/bootstrap.gohtml",
+		"views/layouts/footer.gohtml")
 
 	t, err := template.ParseFiles(files...)
 
@@ -15,10 +17,11 @@ func NewView(files ...string) *View {
 
 	return &View{
 		Template: t,
+		Layout:   layout,
 	}
 }
 
 type View struct {
-	Template * template.Template
+	Template *template.Template
+	Layout   string
 }
-
