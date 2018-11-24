a41990cced1612c61c2783854378a5ad1fe7b956
harrisonbrock <harrison@harrisonbrock.com>
2018-11-23 19:29:29 -0500

    Reactor code to use load all layout files using the layout function

--- views/view.go
+++ views/view.go
@@ -3,14 +3,17 @@ package views
 import (
 	"html/template"
 	"net/http"
+	"path/filepath"
+)
+
+var (
+	LayoutDir   = "views/layouts/"
+	TemplateExt = ".gohtml"
 )
 
 func NewView(layout string, files ...string) *View {
 
-	files = append(files,
-		"views/layouts/bootstrap.gohtml",
-		"views/layouts/navbar.gohtml",
-		"views/layouts/footer.gohtml")
+	files = append(files, layoutFiles()...)
 
 	t, err := template.ParseFiles(files...)
 
@@ -23,6 +26,19 @@ func NewView(layout string, files ...string) *View {
 		Template: t,
 		Layout:   layout,
 	}
+}
+
+// returns a slice of strings of view file names
+func layoutFiles() []string {
+
+	files, err := filepath.Glob(LayoutDir + "*" + TemplateExt)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return files
+
 }
 
 type View struct {
