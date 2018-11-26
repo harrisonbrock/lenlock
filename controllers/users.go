91edd7f66321625c42d0ae693e1253a4fed808e7
harrisonbrock <harrison@harrisonbrock.com>
2018-11-26 18:12:14 -0500

    added error handling

--- controllers/users.go
+++ controllers/users.go
@@ -18,10 +18,13 @@ type Users struct {
 
 // GET /signup
 func (u *Users) New(w http.ResponseWriter, r *http.Request) {
-	u.NewView.Render(w, nil)
+	if err := u.NewView.Render(w, nil); err != nil {
+		panic(err)
+	}
 }
 
 // POST /signup
 func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
+
 	fmt.Fprintln(w, "This is a fake message. Placeholder until database is setup")
 }
